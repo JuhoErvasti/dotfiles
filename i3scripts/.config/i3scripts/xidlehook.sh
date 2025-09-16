daa12d7bb8cbd9299758565666c3cc66c15f32f1
JuhoErvasti <juho.ervasti@gispo.fi>
2025-09-16 15:18:30 +0300

    i3: don't lock when audio is playing

--- i3scripts/.config/i3scripts/xidlehook.sh
+++ i3scripts/.config/i3scripts/xidlehook.sh
@@ -4,4 +4,4 @@ set -e
 
 killall xidlehook
 
-~/.cargo/bin/xidlehook --not-when-fullscreen --timer 180 '~/.config/i3scripts/lock.sh' '' --timer 120 'systemctl suspend' ''
+~/.cargo/bin/xidlehook --not-when-fullscreen --not-when-audio --timer 180 '~/.config/i3scripts/lock.sh' '' --timer 120 'systemctl suspend' ''
