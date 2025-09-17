2a22460af4afc5598164209d38461d4bac560e98
JuhoErvasti <juho.ervasti@gispo.fi>
2025-09-17 19:30:37 +0300

    nixos-migration: remove X-related stuff

--- i3scripts/.config/i3scripts/lock.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -e
-
-export XSECURELOCK_SAVER=saver_mpv
-export XSECURELOCK_LIST_VIDEOS_COMMAND="find ~/Pictures/lock_screen/ -type f"
-
-XSECURELOCK_COMPOSITE_OBSCURER=0 xsecurelock
--- i3scripts/.config/i3scripts/xidlehook.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -e
-
-killall xidlehook
-
-~/.cargo/bin/xidlehook --not-when-fullscreen --not-when-audio --timer 180 '~/.config/i3scripts/lock.sh' '' --timer 120 'systemctl suspend' ''
